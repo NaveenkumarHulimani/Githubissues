f8dd8cb92df35ed065d5b1f22cb0f14f58192dc5
naveenkumarh1998@gmail.com <naveenkumarh1998@gmail.com>
2023-04-03 23:26:50 +0530

    logic for fetching is added and loadPrev and loadNext functions added

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ let issuList=document.getElementById('list');
 function Getissues(page){
     let url=`https://api.github.com/repositories/1296269/issues?page=${page}&per_page=5`;
     
-    fetch(url).then((response)=>response.json)
+    fetch(url).then((response)=>response.json())
     .then(data=>{
         console.log(data);
         issuList.innerHTML="";
-        data.forEach(issu => {
+        data.forEach(issue => {
             let listItem=document.createElement('li');
-            listItem.textContent=issu.title;
+            listItem.textContent=issue.title;
             issuList.appendChild(listItem);
             PageTitle.innerHTML=`PageNumber ${page}`;
         });
@@ -19,3 +19,15 @@ function Getissues(page){
     }).catch(e=>console.error(e))
 }
 Getissues(PageNo);
+
+function loadPrev(){
+    if(PageNo>1){
+        PageNo--;
+        Getissues(PageNo);
+    }
+}
+
+function loadNext(){
+        PageNo++;
+        Getissues(PageNo);
+}
